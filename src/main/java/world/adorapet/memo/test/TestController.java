d727c8d32b60a8a51879f016e4b4bec943f6207b
DESKTOP-KHVJGSC\user <0414soyeon@gmail.com>
2023-03-16 18:13:30 +0900

    [김기동] - 메모 프로젝트 최초 파일 추가

---
+++ src/main/java/world/adorapet/memo/test/TestController.java
@@ -0,0 +1,21 @@
+package world.adorapet.memo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		
+		return "hello World!";
+	}
+	
+	@GetMapping("/hellojsp")
+	public String hellojsp() {
+		return "hello";
+	}
+}
---
+++ src/test/java/world/adorapet/memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package world.adorapet.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
